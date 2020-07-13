d1c4cee3d1510c271f84afaf60ee9d7ef1a64d68
Sagargajare <56780589+Sagargajare@users.noreply.github.com>
2020-07-13 20:39:09 +0530

    13-07

--- app.py
+++ app.py
@@ -1,15 +1,35 @@
-from flask import Flask , render_template,url_for
+from flask import Flask , render_template,url_for ,flash ,redirect
+from forms import RegistrationForm , LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '124fv22f58f5f6f9ff4dwda6d'
+
+
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html')
 
-@app.route("/register")
+@app.route("/register", methods=['GET','POST'])
 def register():
-    return render_template('register.html', title='Registration')
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Registration',form=form)
+
+@app.route("/login", methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'sagar@gmail.com' and form.password.data =='password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Please Check username and Password ','danger')
+    return render_template('login.html', title='Login',form=form)
 
 
 
